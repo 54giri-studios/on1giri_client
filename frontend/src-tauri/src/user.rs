2dde928d64ae37d04ebd705c0a320e18656f7dee
SamuelVanie <samuelmichaelvanie@gmail.com>
2024-04-14 15:26:05 +0200

    feat: user creation logic
    - Add create_user method to be able to create a user by sending a
    request to the server
    
    - fix login's logic, the token is retrieved as response and sent
    to the frontend and should be added to the localStorage

--- frontend/src-tauri/src/user.rs
+++ frontend/src-tauri/src/user.rs
@@ -4,12 +4,11 @@ use std::collections::HashMap;
 #[tauri::command]
 pub async fn login(
     username: String,
-    password: String,
+    password: String
 ) -> Result<result::OperationResult, result::OperationResult> {
-    // I've concerned about the security of this way of handle login's logic
-    // This is boilerplate that should be handle in a safer way
-    // TODO
-    println!("Attempting login");
+
+    log::info!("Logging in user: {}", username);
+
     let endpoint = format!("/login");
 
     let json = {
@@ -28,34 +27,125 @@ pub async fn login(
     }
 
     let result = match utils::build_url(endpoint) {
-        Ok(url) => utils::post_server(url, body).await,
+        Ok(url) => utils::post_server(url, body, None).await,
         Err(e) => Err(e),
     };
-
-    if result.is_ok() {
-        // Save the token to the database
-    }
 
     return result;
 }
 
 
 #[tauri::command]
-pub async fn get_user_info(user_id: i32) -> Result<result::OperationResult, result::OperationResult> {
+pub async fn create_user(username: String, email: String, description: String, picture: String) -> Result<result::OperationResult, result::OperationResult> {
+    let endpoint = "/user/create";
+
+    let mut body = HashMap::new();
+    body.insert("username", username);
+    body.insert("email", email);
+    body.insert("description", description);
+    body.insert("picture", picture);
+
+    if let Ok(body) = utils::convert_to_json_str(body) {
+        match utils::build_url(endpoint) {
+            Ok(u) => utils::post_server(u, body, None).await,
+            Err(e) => Err(e)
+        }
+    } else {
+        return Err(result::OperationResult::new("Cannot convert these values to json string".to_string(), result::ResultCode::ERROR));
+    }
+}
+
+
+#[tauri::command]
+pub async fn get_user_info(user_id: i32, token: String) -> Result<result::OperationResult, result::OperationResult> {
     let endpoint = format!("/user/{}", user_id);
 
     match utils::build_url(endpoint) {
-        Ok(url) => utils::fetch_data(url).await,
+        Ok(url) => utils::fetch_data(url, token).await,
         Err(e) => Err(e),
     }
 }
 
 #[tauri::command]
-pub async fn get_user_guilds(user_id: i32) -> Result<result::OperationResult, result::OperationResult> {
+pub async fn get_user_guilds(user_id: i32, token: String) -> Result<result::OperationResult, result::OperationResult> {
     let endpoint = format!("/user/{}/guilds", user_id);
-    println!("Getting user's guilds");
     match utils::build_url(endpoint) {
-        Ok(url) => utils::fetch_data(url).await,
+        Ok(url) => utils::fetch_data(url, token).await,
         Err(e) => Err(e),
     }
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use httpmock::prelude::*;
+
+
+    #[tokio::test]
+    async fn should_retrieve_user_guilds_if_exists() {
+        let server = MockServer::start();
+        std::env::set_var("SERVER_URL", server.base_url());
+
+        let _m = server.mock(|when, then| {
+            when.method(GET).path("/user/1/guilds");
+            then.status(200)
+                .header("Content-Type", "application/json")
+                .body("{ \"guilds\": [ { \"id\": 1, \"name\": \"Guild 1\" }, { \"id\": 2, \"name\": \"Guild 2\" } ] }");
+        });
+
+        match get_user_guilds(1, "flkdjsalfkjsdlfk2857625".into()).await {
+            Ok(res) => {
+                assert_eq!(res.code, result::ResultCode::SUCCESS);
+                assert_eq!(res.content, "{ \"guilds\": [ { \"id\": 1, \"name\": \"Guild 1\" }, { \"id\": 2, \"name\": \"Guild 2\" } ] }");
+            },
+            Err(e) => { panic!("{e:?}"); }
+        }
+    }
+
+
+    #[tokio::test]
+    async fn should_retrieve_user_info_if_exists() {
+        let server = MockServer::start();
+        std::env::set_var("SERVER_URL", server.base_url());
+
+        let _m = server.mock(|when, then| {
+            when.method(GET).path("/user/1");
+            then.status(200)
+                .header("Content-Type", "application/json")
+                .body("{ \"id\": 1, \"username\": \"test\", \"email\": \"user@gmail.com\"}");
+        });
+        
+        match get_user_info(1, String::from("fldjsafkljsadlkfj29527u5")).await {
+            Ok(res) => {
+                assert_eq!(res.code, result::ResultCode::SUCCESS);
+                assert!(res.content == "{ \"id\": 1, \"username\": \"test\", \"email\": \"user@gmail.com\"}");
+            }
+            Err(e) => { panic!("{e:?}"); }
+        }
+    }
+
+
+    #[tokio::test]
+    async fn should_create_user(){
+        let server = MockServer::start();
+        std::env::set_var("SERVER_URL", server.base_url());
+
+        let _m = server.mock(|when, then| {
+            when.method(GET).path("/user/create");
+            then.status(200)
+                .header("Content-Type", "application/json")
+                .body("{ \"token\" : \"fljdasf85425fklhafasflas\" }");
+        });
+
+        match create_user("username".into(), "user@gmail.com".into(), "Etudiant en info".into(), "user23457525.png".into()).await {
+            Ok(res) => {
+                assert_eq!(res.code, result::ResultCode::SUCCESS);
+                assert_eq!(res.content, "{ \"token\" : \"fljdasf85425fklhafasflas\" }");
+            }
+            Err(e) => { panic!("{e:?}"); }
+        }
+    }
+
+
 }

93f955d3ca1211e2bec3cb018ad4ab8958345290
origaniels <origaniels@gmail.com>
2024-04-16 12:17:21 +0200

    added userbanner and button classes, split css again

--- frontend/ui/ts/User.js
+++ frontend/ui/ts/User.js
@@ -1,6 +1,39 @@
 class UserBanner {
-    constructor(name, connectionStatus) {
+    constructor(name, connectionStatus, connectionMessage) {
         this.name = name;
         this.connectionStatus = connectionStatus;
+        this.connectionMessage = connectionMessage;
+    }
+
+    display() {
+        let bannerSuperWrapper = document.createElement("div");
+        bannerSuperWrapper.className = "popupForm";
+
+        let bannerWrapper = document.createElement("div");
+        bannerWrapper.className = "popupInnerWrapper";
+
+        return bannerSuperWrapper;
     }
 }
+
+class UserButton {
+    constructor(name, userId, connectionStatus, connectionMessage) {
+        this.userId = userId;
+        this.name = name;
+        this.connectionStatus = connectionStatus;
+        this.connectionMessage = connectionMessage;
+    }
+
+    display() {
+        let button = document.createElement("div");
+        button.className = "userButton";
+        button.addEventListener("click", this.displayBanner, false);
+        return button;
+    }
+
+    displayBanner() {
+        let banner = new UserBanner(this.name, this.connectionStatus, this.connectionMessage);
+        document.appendChild(banner.display());
+    }
+}
+

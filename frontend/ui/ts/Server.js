62c99070c837a8a9dee27871474b89298b011689
niels <niels@ubuntu.myguest.virtualbox.org>
2024-04-19 13:56:27 +0200

    added minimalist user banners

--- frontend/ui/ts/Server.js
+++ frontend/ui/ts/Server.js
@@ -1,7 +1,9 @@
 async function createServer(e, name) {
     e.preventDefault();
+    // invoke the createServer
 }
 
 async function createChannel(e, name) {
     e.preventDefault();
+    // invoke the createChannel
 }
--- frontend/ui/ts/User.js
+++ frontend/ui/ts/User.js
@@ -8,9 +8,29 @@ class UserBanner {
     display() {
         let bannerSuperWrapper = document.createElement("div");
         bannerSuperWrapper.className = "popupForm";
+        bannerSuperWrapper.style.display = "flex";
+        bannerSuperWrapper.addEventListener("click", async ()=>{
+            bannerSuperWrapper.remove();
+        });
 
         let bannerWrapper = document.createElement("div");
         bannerWrapper.className = "popupInnerWrapper";
+        bannerWrapper.addEventListener("click", async (e)=>{
+            e.stopPropagation();
+        });
+        bannerSuperWrapper.appendChild(bannerWrapper);
+
+        let bannerTop = document.createElement("div");
+        bannerTop.className = "bannerTop";
+        let name = document.createElement("h2");
+        name.innerText = this.name;
+        let status = document.createElement("p");
+        status.innerText = this.connectionMessage;
+        bannerTop.appendChild(name);
+        bannerTop.appendChild(status);
+
+        bannerWrapper.appendChild(bannerTop);
+
 
         return bannerSuperWrapper;
     }
@@ -27,7 +47,7 @@ class UserButton {
     display() {
         let button = document.createElement("div");
         button.className = "userButton";
-        button.addEventListener("click", this.displayBanner, false);
+        button.addEventListener("click", async ()=>this.displayBanner(this.name, this.connectionStatus, this.connectionMessage), false);
 
         let usernameWrapper = document.createElement("div");
         usernameWrapper.className = "usernameWrapper";
@@ -42,8 +62,9 @@ class UserButton {
         return button;
     }
 
-    displayBanner() {
-        let banner = new UserBanner(this.name, this.connectionStatus, this.connectionMessage);
+    displayBanner(name, status, message) {
+        console.log(this.name);
+        let banner = new UserBanner(name, status, message);
         body.firstElementChild.appendChild(banner.display());
     }
 }
